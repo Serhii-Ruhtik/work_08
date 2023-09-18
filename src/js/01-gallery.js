43406a2462803d88866a7d09e82681e9fe9e8799
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-09-18 22:06:41 +0300

    prakt

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,8 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
